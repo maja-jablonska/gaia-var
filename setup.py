c6460b757a1cc6d90a54f5d7c28ef5b931f0a8f5
Maja Jabłońska <majajjablonska@gmail.com>
2023-03-15 14:31:16 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gaia_var',
-    version='0.9.0',
+    version='0.9.1',
     packages=find_packages(include=['gaia_var', 'gaia_var.*']),
         install_requires=[
         'astropy~=5.1',
